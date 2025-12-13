12c0083534f9b913da6b6a6df90129958937123d
Tristan Pemble <tpemble@gmail.com>
2025-12-13 13:59:50 -0800

    add nixgl wrapper to godot in nix flake

--- flake.nix
+++ flake.nix
@@ -6,11 +6,14 @@
     zls.url = "github:zigtools/zls";
     zls.inputs.nixpkgs.follows = "nixpkgs";
     zls.inputs.zig-overlay.follows = "zig";
+    nixgl.url = "github:nix-community/nixGL";
+    nixgl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       self,
+      nixgl,
       nixpkgs,
       zig,
       zls,
@@ -27,6 +30,7 @@
         system:
         import nixpkgs {
           inherit system;
+          overlays = [ nixgl.overlay ];
         }
       );
     in
@@ -40,12 +44,21 @@
             buildInputs = [ zigPinned ];
           });
           inherit (pkgs) lib stdenv;
+
+          # Wrap godot with nixGL for non-NixOS systems (Mesa for AMD/Intel)
+          godotWrapped =
+            if stdenv.hostPlatform.isLinux then
+              pkgs.writeShellScriptBin "godot" ''
+                exec ${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa ${pkgs.godot}/bin/godot "$@"
+              ''
+            else
+              pkgs.godot;
         in
         {
           default = pkgs.mkShell {
             buildInputs =
               [
-                pkgs.godot
+                godotWrapped
                 pkgs.lldb
                 zigPinned
                 zlsPinned
