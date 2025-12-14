045f56e7c2bbfc9f07a80ade452b5bfc555dd951
Tristan Pemble <tpemble@gmail.com>
2025-12-13 16:33:10 -0800

    fix example project

--- example/build.zig
+++ example/build.zig
@@ -1,57 +1,54 @@
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *Build) !void {
+    // Options
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const godot_path = b.option([]const u8, "godot", "Path to Godot engine binary [default: `godot`]") orelse "godot";
 
-    const gdzig_dep = b.dependency("gdzig", .{
+    const godot_version = b.option([]const u8, "godot", "Which version of Godot to generate bindings for [default: `4.5.1`]") orelse "4.5.1";
+    const godot_exe = godot.executable(b, b.graph.host, godot_version) orelse return;
+
+    // Dependencies
+    const gdzig = b.dependency("gdzig", .{
         .target = target,
         .optimize = optimize,
-        .godot = godot_path,
-
-        // This is the default value, so could be omitted. If you want, you can change it to "double".
-        // You should hardcode this for your project, rather than exposing it as a build option like the godot path.
-        .precision = @as([]const u8, "float"),
+        .godot = godot_version,
     });
 
+    // Module
     const mod = b.createModule(.{
         .root_source_file = b.path("src/example.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "gdzig", .module = gdzig.module("gdzig") },
+        },
     });
-    mod.addImport("gdzig", gdzig_dep.module("gdzig"));
 
+    // Library
     const lib = b.addLibrary(.{
-        .linkage = .dynamic,
         .name = "example",
+        .linkage = .dynamic,
         .root_module = mod,
         .use_llvm = true,
     });
 
-    const out_path = "../project/lib";
-    // b.lib_dir = "./project/lib";
-    const install = b.addInstallArtifact(lib, .{ .dest_dir = .{
-        .override = .{ .custom = out_path },
-    } });
-
+    // Install
+    const install = b.addInstallArtifact(lib, .{
+        .dest_dir = .{ .override = .{ .custom = "../project/lib" } },
+    });
     b.default_step.dependOn(&install.step);
 
-    const project_path = b.path("./project");
-    const load_cmd = b.addSystemCommand(&.{
-        godot_path, "--headless", "--quit", "--editor", "--path",
-    });
-    load_cmd.addDirectoryArg(project_path);
-    //load_cmd.expectExitCode(0);
-    load_cmd.step.dependOn(b.getInstallStep());
-    const load_step = b.step("load", "Load project");
-    load_step.dependOn(&load_cmd.step);
+    // Run
+    const run = Build.Step.Run.create(b, "run godot");
+    run.addFileArg(godot_exe);
+    run.addArg("--path");
+    run.addDirectoryArg(b.path("./project"));
+    run.step.dependOn(&install.step);
 
-    const run_cmd = b.addSystemCommand(&.{
-        godot_path, "--path",
-    });
-    run_cmd.addDirectoryArg(project_path);
-    run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run with Godot");
-    run_step.dependOn(&run_cmd.step);
+    run_step.dependOn(&run.step);
 }
 
 const std = @import("std");
+const Build = std.Build;
+
+const godot = @import("godot");
